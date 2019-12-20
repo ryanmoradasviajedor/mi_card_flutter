3ade33c408c30ad9b80f05ec445b03183d906cee
Ryan Viajedor <ryanviajedor@Ryans-MacBook-Pro.local>
2019-12-20 21:48:18 +0800

    Tapping into Widget Properties

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,19 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ryan.jpg'),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.blue,
-                child: Text("Container 2"),
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.red,
-                child: Text("Container 3"),
+              Text(
+                'Ryan Viajedor',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
